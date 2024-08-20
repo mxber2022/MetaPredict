abafcab69d9c7d6c2f12ec9470560d290bf35754
mxber2022 <mxber2022@gmail.com>
2024-08-20 04:56:57 +0200

    v1

--- AIModel/app.py
+++ AIModel/app.py
@@ -2,14 +2,18 @@ from flask import Flask, request, jsonify
 from langchain_openai import OpenAI
 from langchain.sql_database import SQLDatabase
 from langchain_experimental.sql import SQLDatabaseChain
-from apikey import apikey
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
 
 app = Flask(__name__)
 
 # Initialize the database and LLM
 dburi = "sqlite:///testDatabase.db" 
 db = SQLDatabase.from_uri(dburi)
-llm = OpenAI(temperature=0, api_key=apikey)
+llm = OpenAI(temperature=0, api_key=os.getenv('OPENAI_API_KEY'))
 db_chain = SQLDatabaseChain(llm=llm, database=db, verbose=True)
 
 @app.route('/query', methods=['POST'])
