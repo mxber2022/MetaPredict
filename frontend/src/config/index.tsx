f2bbbdf2ed41ac425e3091572002138c2863acea
mxber2022 <mxber2022@gmail.com>
2024-08-20 08:09:31 +0200

    Update index.tsx

--- frontend/src/config/index.tsx
+++ frontend/src/config/index.tsx
@@ -1,7 +1,7 @@
 import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
 
 import { cookieStorage, createStorage } from 'wagmi'
-import { mainnet, sepolia, baseSepolia, modeTestnet, hederaTestnet } from 'wagmi/chains'
+import { mainnet, sepolia, baseSepolia, modeTestnet, hederaTestnet, rootstockTestnet } from 'wagmi/chains'
 
 export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
 
@@ -10,12 +10,12 @@ if (!projectId) throw new Error('Project ID is not defined')
 export const metadata = {
   name: 'Web3Modal',
   description: 'Web3Modal Example',
-  url: 'https://web3modal.com', // origin must match your domain & subdomain
+  url: 'https://web3modal.com', 
   icons: ['https://avatars.githubusercontent.com/u/37784886']
 }
 
 // Create wagmiConfig
-const chains = [baseSepolia, modeTestnet, hederaTestnet, sepolia] as const
+const chains = [rootstockTestnet] as const
 export const config = defaultWagmiConfig({
   chains,
   projectId,
