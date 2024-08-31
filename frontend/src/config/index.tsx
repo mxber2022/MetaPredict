460eb553f00c0b8296762c1a5b2625a26d757dc7
mxber2022 <mxber2022@gmail.com>
2024-08-31 02:17:34 +0200

    UI update

--- frontend/src/config/index.tsx
+++ frontend/src/config/index.tsx
@@ -1,7 +1,7 @@
 import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
 
 import { cookieStorage, createStorage } from 'wagmi'
-import { mainnet, sepolia, baseSepolia, modeTestnet, hederaTestnet, rootstockTestnet } from 'wagmi/chains'
+import { mainnet, sepolia, baseSepolia, modeTestnet, hederaTestnet, rootstockTestnet, optimismSepolia, arbitrumSepolia } from 'wagmi/chains'
 
 export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
 
@@ -15,7 +15,7 @@ export const metadata = {
 }
 
 // Create wagmiConfig
-const chains = [sepolia, rootstockTestnet] as const
+const chains = [sepolia, rootstockTestnet, optimismSepolia, arbitrumSepolia] as const
 export const config = defaultWagmiConfig({
   chains,
   projectId,
